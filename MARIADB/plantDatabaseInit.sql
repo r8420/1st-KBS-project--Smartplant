1b371ea69e6d5db499e93cf57b2382059b390a91
sanderpol <sander__pol@outlook.com>
2019-09-10 10:08:44 +0200

    Create plantDatabaseInit.sql

---
+++ MARIADB/plantDatabaseInit.sql
@@ -0,0 +1,20 @@
+DROP USER IF EXISTS 'smartplant'@'localhost';
+DROP DATABASE IF EXISTS smartplant;
+CREATE DATABASE smartplant;
+USE smartplant;
+CREATE TABLE planten(
+  naam CHAR(20),
+  temp CHAR (5),
+  licht CHAR(5),
+  vocht CHAR(5),
+  PRIMARY KEY (name)
+);
+INSERT INTO planten (
+naam, temp, licht, vocht)
+VALUES (lepelplant, 16, 2.5, 70);
+INSERT INTO planten (naam, temp, licht, vocht) VALUES (gatenplant, 20, 3.0, 60);
+INSERT INTO planten (naam, temp, licht, vocht) VALUES (calathea, 17, 2, 50);
+INSERT INTO planten (naam, temp, licht, vocht) VALUES (kwartjesplant, 12, 2, 55);
+INSERT INTO planten (naam, temp, licht, vocht) VALUES (lepelplant,16,2.5,70);
+CREATE USER 'smartplant'@'localhost' IDENTIFIED BY 'planten';
+GRANT INSERT ON smartplant.planten TO 'smartplant'@'localhost';
