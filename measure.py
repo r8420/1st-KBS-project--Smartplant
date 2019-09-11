5d1d0ef213b03a8c10cdd475c698688a8a9ac4b8
pekka26 <55087185+pekka26@users.noreply.github.com>
2019-09-11 13:09:40 +0200

    Update measure.py

--- measure.py
+++ measure.py
@@ -15,17 +15,19 @@
 #
 
 # import some modules
+import datetime
 import sys
 import getopt
 import sense_hat
 import time
+
 import mysql.connector as mariadb
 from mysql.connector import errorcode
 
 
+
 def stop():
     exit()
-
 # sensor name
 sensor_name_1 = 'Temperatuur'
 sensor_name_2 = 'Luchtvochtigheid'
@@ -33,16 +35,16 @@ sensor_name_3 = 'Licht'
 sensor_name_4 = 'Voedingsstoffen'
 # database connection configuration
 dbconfig = {
-    'user': 'CHANGE',
-    'password': 'CHANGE',
+    'user': 'sensem',
+    'password': 'h@',
     'host': 'localhost',
-    'database': 'weerstation',
+    'database': 'smartplant',
     'raise_on_warnings': True,
 }
 
 # parse arguments
 verbose = True
-interval = 60 # second
+interval = 10 # second
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "vt:")
@@ -62,6 +64,46 @@ for opt, arg in opts:
 # instantiate a sense-hat object
 sh = sense_hat.SenseHat()
 
+# instantiate a database connection
+try:
+    mariadb_connection = mariadb.connect(**dbconfig)
+    if verbose:
+        print("Database connected")
+
+except mariadb.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print("Error: {}".format(err))
+    sys.exit(2)
+
+# create the database cursor for executing SQL queries
+cursor = mariadb_connection.cursor(buffered=True)
+
+try:
+    print("Vul de id van deze sensor in:")
+    input_id = input()
+    cursor.execute("SELECT naam FROM meting WHERE id=%s", [input_id])
+except mariadb.Error as err:
+    print("Error: {}".format(err))
+    sys.exit(2)
+row_count = cursor.rowcount
+if row_count == 0:
+
+    print("Vul de naam van deze sensor in:")
+    input_name = input()
+    print("Er wordt een sensor toegevoegd.")
+    try:
+        cursor.execute('INSERT INTO meting (naam, id) VALUES (%s, %s);', (input_name, input_id))
+    except mariadb.Error as err:
+        print("Error: {}".format(err))
+    mariadb_connection.commit()
+    print("ok.")
+
+
+
 # infinite loop
 try:
     while True:
@@ -69,6 +111,7 @@ try:
         for x in sh.stick.get_events():
             if x.direction == 'middle':
                 exit()
+
         # instantiate a database connection
         try:
             mariadb_connection = mariadb.connect(**dbconfig)
@@ -83,46 +126,15 @@ try:
             else:
                 print("Error: {}".format(err))
             sys.exit(2)
-
         # create the database cursor for executing SQL queries
         cursor = mariadb_connection.cursor(buffered=True)
 
         # turn on autocommit
         #cursor.autocommit = True
 
-        # get the sensor_id for temperature sensor
-        try:
-            cursor.execute("SELECT id FROM sensor WHERE naam=%s", [sensor_name_1])
-        except mariadb.Error as err:
-            print("Error: {}".format(err))
-            sys.exit(2)
-
-        sensor_id = [0,1,2]
-        sensor_id[0] = cursor.fetchone()
-        if sensor_id[0] is None:
-            print("Error: no sensor found with naam = %s" % sensor_name_1)
-            sys.exit(2)
-
-        if verbose:
-            print("Reading data from sensor %s with id %s" % (sensor_name_1, sensor_id[0][0]))
-
-        # get the sensor_id for humidity sensor
-        try:
-            cursor.execute("SELECT id FROM sensor WHERE naam=%s", [sensor_name_2])
-        except mariadb.Error as err:
-            print("Error: {}".format(err))
-            sys.exit(2)
-
-        sensor_id[1] = cursor.fetchone()
-        if sensor_id[1] is None:
-            print("Error: no sensor found with naam = %s" % sensor_name_2)
-            sys.exit(2)
-
-        if verbose:
-            print("Reading data from sensor %s with id %s" % (sensor_name_2, sensor_id[1][0]))
 
         # measure temperature
-        temp = round(sh.get_temperature(), 1)
+        temp = round(sh.get_temperature(), 2)
 
         tFile = open('/sys/class/thermal/thermal_zone0/temp')
         tempf = float(tFile.read())
@@ -131,26 +143,28 @@ try:
             temp = temp-18.5
         else:
             temp = temp-5
-
-        # print temperature
-        sh.set_rotation(180)
-        sh.show_message("%.1f C" % temp, scroll_speed=0.10, text_colour=[0, 255, 0])
+        temp = round(temp, 2)
 
         # measure humidity
-        humidity = round(sh.get_humidity(), 1)
+        humidity = round(sh.get_humidity(), 2)
 
+        # calculate light hours
+        datetime_now = datetime.datetime.now()
+        struct_time_sunrise = time.strptime(datetime.datetime.now().strftime('%Y/%m/%d 07:04:00'),"%Y/%m/%d %H:%M:%S")
+        datetime_sunrise = datetime.datetime.fromtimestamp(time.mktime(struct_time_sunrise))
+
+        difference = datetime_now-datetime_sunrise
+        light = round(difference.total_seconds() / 3600,2)
 
         # verbose
         if verbose:
             print("Temperature: %s C" % temp)
             print("Humidity: %s %%" % humidity)
+            print("Light hours: %s hours" % light)
 
         # store measurement in database
         try:
-            if temp is not 0:
-                cursor.execute('INSERT INTO meting (waarde, sensor_id) VALUES (%s, %s);', (temp, sensor_id[0][0]))
-            if humidity is not 0:
-                cursor.execute('INSERT INTO meting (waarde, sensor_id) VALUES (%s, %s);', (humidity, sensor_id[1][0]))
+            cursor.execute('UPDATE meting SET temp=%s, licht=%s, vocht=%s;', (temp, light, humidity))
         except mariadb.Error as err:
             print("Error: {}".format(err))
 
